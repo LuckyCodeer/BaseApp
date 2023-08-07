58b9f1a1cb652799338fd3dbf54e8fb7d5587d03
杨宏伟 <yanghongwei@huodada.com>
2023-08-07 14:30:09 +0800

    完善MVVM

--- lib-common/src/main/java/com/lib_common/base/mvvm/BaseViewModel.java
+++ lib-common/src/main/java/com/lib_common/base/mvvm/BaseViewModel.java
@@ -6,7 +6,6 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.DefaultLifecycleObserver;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
 /**
  * 基类ViewModel
